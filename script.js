f47605359d4fab800cd164c52e7d30e392cc49b2
OrlandoMoralesKuan <omoralesk@hotmail.com>
2024-02-08 17:10:21 +0000

    Writing code in script.js for solving exercise 100

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  for (episode of episodeList) {
+    const movieList = document.getElementById("root").content.cloneNode(true);
+    movieList.querySelector("h1").textContent = episode.name;
+    const seasonCode = document.createElement("span");
+    seasonCode.textContent = ` - S${String(episode.season).padStart(2, "0")}`;
+    movieList.querySelector("h1").append(seasonCode);
+    const episodeCode = document.createElement("span");
+    episodeCode.textContent = `E${String(episode.number).padStart(2, "0")}`;
+    movieList.querySelector("h1").append(episodeCode);
+    movieList.querySelector("img").setAttribute("src", episode.image.medium);
+    movieList.querySelector("p").textContent = episode.summary;
+    document.body.append(movieList);
+  }
 }
-
 window.onload = setup;
